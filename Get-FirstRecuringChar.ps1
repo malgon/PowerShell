298889df517a75d0294ab10de1681d4a53c4c9a9
Merav <m_algon@hotmail.com>
2020-11-07 18:18:52 -0800

    Create Get-FirstRecuringChar.ps1

---
+++ Get-FirstRecuringChar.ps1
@@ -0,0 +1,26 @@
+﻿function Get-FirstRecuringChar{
+
+    param (
+        [parameter(Mandatory = $true)] [string]$foo
+    )
+    
+    $location = $foo.Length
+
+    for($i=0; $i -le $foo.Length; $i++){
+        $recurring = 0
+        $letter=$foo[$i]
+        for($x=$i+1;$x -le $foo.Length; $x++){
+            if($letter -eq $foo[$x]){
+                $recurring = $x;
+                break;
+            }
+        } 
+        if ($recurring -ne 0){
+            if($recurring -lt $location){
+                $location = $recurring
+            }
+        }
+    }
+    Write-Output $foo[$location]
+         
+}
